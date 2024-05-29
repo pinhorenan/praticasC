4cb3aeeaefeb7cac9c521fd654ab2ca691cfaa6a
Renan Pinho <pinhorenan@icloud.com>
2024-05-29 16:13:34 -0300

    Adicionar pontuação ao jogo da adivinhação em C++

--- adivinha_numero.cpp
+++ adivinha_numero.cpp
@@ -7,6 +7,7 @@ int main() {
     int contador = 0;
     int chute = 0;
     bool acertou = false;
+    double pontos = 1000.0;
 
     cout << "*************************************" << endl;
     cout << "* Bem-vindo ao jogo da adivinhação! *" << endl;
@@ -21,18 +22,22 @@ int main() {
 
         if (chute > SEGREDO) {
         contador++;
+        pontos -= abs(chute - SEGREDO)/2.0;
         cout << "Que pena! " << chute << " é maior que o número secreto!" << endl;
         } else {
         contador++;
+        pontos -= abs(chute - SEGREDO)/2.0;
         cout << "Que pena! " << chute << " é menor que o número secreto!" << endl;
         }
     }
     
     if (acertou) {
         contador = 0;
-        cout << "Parabéns! Você acertou o número secreto!" << endl;
+        cout.precision(2);
+        cout << fixed;
+        cout << "Parabéns! Você acertou o número secreto! Sua pontuação foi: " << pontos << " pontos." << endl;
     } else {
-        cout << "Que pena! Você perdeu! O número secreto era " << SEGREDO << endl;
+        cout << "Que pena! Você perdeu! O número secreto era " << SEGREDO << "." << endl;
     }
 
     return 0;
