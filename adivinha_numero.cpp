1ff50344df49b4678ff20942ed2aa2dd04756fd9
Renan Pinho <pinhorenan@icloud.com>
2024-05-29 18:25:14 -0300

    Adicionar opção de escolha de nível de dificuldade no jogo da adivinhação e números aleatórios.

--- adivinha_numero.cpp
+++ adivinha_numero.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
-    const int SEGREDO = 42;
-    const int MAX_TENTATIVAS = 5;
-    int contador = 0;
-    int chute = 0;
+    srand(time(NULL));
+    const int SEGREDO = rand() % 100;
+    
+    int max_tentativas;
+    int chute;
+    char dificuldade;
+
+    int tentativas = 0;
     bool acertou = false;
     double pontos = 1000.0;
 
@@ -13,7 +19,35 @@ int main() {
     cout << "* Bem-vindo ao jogo da adivinhação! *" << endl;
     cout << "*************************************" << endl;
 
-    while (!acertou && contador < MAX_TENTATIVAS)
+    cout << "Escolha o nível de dificuldade: " << endl;
+    cout << "Fácil (F), Médio (M) ou Difícil (D): ";
+
+    cin >> dificuldade;
+
+    switch (dificuldade) {
+        case 'F':
+            cout << "Você escolheu o nível fácil!" << endl;
+            cout << "Você terá 15 tentativas para acertar o número secreto." << endl;
+            cout << "Boa sorte!" << endl;
+            break;
+        case 'M':
+            cout << "Você escolheu o nível médio!" << endl;
+            cout << "Você terá 10 tentativas para acertar o número secreto." << endl;
+            cout << "Boa sorte!" << endl;
+            break;
+        case 'D':
+            cout << "Você escolheu o nível difícil!" << endl;
+            cout << "Você terá 5 tentativas para acertar o número secreto." << endl;
+            cout << "Boa sorte!" << endl;
+            break;
+        default:
+            cout << "Opção inválida! O nível de dificuldade foi definido como médio." << endl;
+            cout << "Você terá 10 tentativas para acertar o número secreto." << endl;
+            cout << "Boa sorte!" << endl;
+            break;
+    }
+
+    while (!acertou && tentativas < max_tentativas)
     {
         cout << "Digite o seu chute: ";
         cin >> chute;
@@ -21,21 +55,21 @@ int main() {
         acertou = (chute == SEGREDO);
 
         if (chute > SEGREDO) {
-        contador++;
+        tentativas++;
         pontos -= abs(chute - SEGREDO)/2.0;
         cout << "Que pena! " << chute << " é maior que o número secreto!" << endl;
         } else {
-        contador++;
+        tentativas++;
         pontos -= abs(chute - SEGREDO)/2.0;
         cout << "Que pena! " << chute << " é menor que o número secreto!" << endl;
         }
     }
     
     if (acertou) {
-        contador = 0;
+        cout << "Parabéns! Você acertou o número secreto em " << tentativas << " tentativas!" << endl;
         cout.precision(2);
         cout << fixed;
-        cout << "Parabéns! Você acertou o número secreto! Sua pontuação foi: " << pontos << " pontos." << endl;
+        cout << "Sua pontuação foi: " << pontos << " pontos." << endl;
     } else {
         cout << "Que pena! Você perdeu! O número secreto era " << SEGREDO << "." << endl;
     }
